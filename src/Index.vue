96aceac5a3293538f93b3a3f90539ce90f36877b
kristjan suursoho <kristjan.suursoho@gmail.com>
2024-07-28 19:06:44 +0300

    update

--- src/Index.vue
+++ src/Index.vue
@@ -1,193 +1,96 @@
 <template>
   <main
-    data-scroll-container
-    class="flex flex-col bg-stone-100 text-stone-900"
-    ref="container"
+    class="prose brose-base flex flex-col text-black max-w-4xl w-full mx-auto py-8 md:py-12 lg:py-24 prose-p:mb-2 prose-p:mt-1 prose-h2:mb-2 prose-h3:mb-1 prose-h3:mt-3 prose-h4:mb-1 prose-h4:mt-0 prose-p:leading-6 prose-ul:my-0 prose-li:my-0 prose-li:marker:text-black px-8 md:px-12"
   >
-    <div class="flex flex-col md:pt-6 2xl:pt-24">
-      <header
-        data-scroll-section
-        class="flex flex-col max-w-screen-2xl w-full mx-auto px-6 md:px-12 md:gap-4"
-      >
-        <div
-          class="flex flex-col md:flex-row md:items-end border-b-2 pb-6 border-stone-900 pt-6"
-        >
-          <h1 class="text-6xl md:text-8xl md:-ml-[2px] font-medium">
-            Kristjan
-            <br />
-            Suursoho
-          </h1>
+    <header class="text-center">
+      <div class="flex items-center flex-wrap">
+        <div class="max-sm:hidden flex-1 text-left">
+          <p>Tallinn, Estonia</p>
+        </div>
 
-          <div class="flex-1"></div>
-
-          <div
-            class="flex md:items-end flex-col max-md:pt-6 md:pb-2 text-base md:text-lg"
+        <div class="flex-1 text-center max-md:text-left">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/kristjan-suursoho-a475ba166/"
           >
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/kristjan-suursoho-a475ba166/"
-              class="flex items-center gap-6 md:gap-4 group hover:text-blue-800"
-            >
-              <span class="material-symbols-outlined text-lg pt-[1px]">
-                link
-              </span>
-              <span class="group-hover:underline">LinkedIn</span>
-            </a>
-
-            <a
-              href="mailto:kristjan.suursoho@gmail.com"
-              class="flex items-center gap-6 md:gap-4 group hover:text-blue-800"
-            >
-              <span class="material-symbols-outlined text-lg pt-[1px]">
-                email
-              </span>
-              <span class="group-hover:underline">
-                kristjan.suursoho@gmail.com
-              </span>
-            </a>
-          </div>
+            <span class="group-hover:underline">LinkedIn</span>
+          </a>
         </div>
-      </header>
 
-      <section
-        data-scroll-section
-        class="max-w-screen-2xl w-full mx-auto md:gap-6 pt-6 md:pt-12 px-6 md:px-12"
-      >
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12">
-          <p class="col-span-1 text-xl md:text-2xl md:leading-[1.4] questrial">
-            With extensive experience collaborating with various startups and
-            development agencies, I specialize in building hybrid applications,
-            full-stack solutions, custom websites, and solution architecture.
-          </p>
-
-          <p class="flex-1 text-base md:text-lg">
-            My approach to work is versatile and adaptable, no task is beneath
-            or above me, embracing tasks of all levels of complexity with
-            dedication and a willingness to learn.
-            <br /><br />
-
-            In addition to my development expertise, I have a strong interest in
-            digital design, engineering, analog music production, philosophy,
-            and art.
-            <br /><br />
-
-            These diverse interests enrich my professional perspective and fuel
-            my creativity in problem-solving.
-          </p>
+        <div class="flex-1 text-right">
+          <a href="mailto:kristjan.suursoho@gmail.com">
+            <span class="group-hover:underline">
+              kristjan.suursoho@gmail.com
+            </span>
+          </a>
         </div>
-      </section>
-    </div>
+      </div>
+      <div class="py-6 border-y border-black">
+        <h1 class="mb-0.5">Kristjan Suursoho</h1>
+      </div>
+      <div class="text-center md:px-6 pt-2">
+        <p>
+          For the last decade or so I've been heavily involved with building
+          start-ups and working with development agencies. During this time I've
+          been lucky to acquire a handful of skills while creating scalable and
+          reliable solutions for the web abd mobile. On my off days I'm drawn to
+          analog music, engineering, philosophy, and art. I hope to encounter a
+          position which allows me to build upon the abilities so far acquired.
+        </p>
+      </div>
+    </header>
 
-    <section
-      data-scroll-section
-      class="flex flex-col max-w-screen-2xl w-full mx-auto md:gap-6 pt-24 md:pt-24 px-6 md:px-12 md:gap-4"
-    >
-      <div
-        v-for="skill in listOfSkills"
-        class="flex flex-col gap-2 lg:gap-6 grid-cols-2"
-      >
-        <h5 class="text-4xl md:text-5xl leading-[1.2] mb-4">
-          {{ skill.title }}
-        </h5>
+    <section class="divide-y divide-black">
+      <h2>Skills and ambitions</h2>
+      <div class="flex flex-col gap-2 pt-2">
+        <div v-for="skill in listOfSkills">
+          <h3>{{ skill.title }}</h3>
+          <p>{{ skill.description }}</p>
 
-        <div
-          class="grid grid-cols-1 md:grid-cols-2 flex-col gap-6 md:gap-12 flex-1"
-        >
-          <div class="grid grid-cols-1 gap-4">
+          <div v-for="extra in skill.listOfExtra" class="pt-1 flex flex-wrap">
+            <p class="mr-2 !mb-0 !mt-0">{{ extra.title }}</p>
             <p
-              v-for="description in skill.listOfDescription"
-              class="text-base md:text-lg"
+              class="mr-2 !mb-0 !mt-0 italic"
+              v-for="(item, index) in extra.list"
             >
-              {{ description }}
+              <span
+                >{{ item }}{{ index < extra.list.length - 1 ? ',' : '.' }}</span
+              >
             </p>
           </div>
-
-          <div class="grid grid-cols-1 gap-2">
-            <template v-for="extra in skill.listOfExtra">
-              <div class="flex flex-col">
-                <h4 class="text-base md:text-lg">
-                  {{ extra.title }}
-                </h4>
-
-                <div class="text-base flex flex-wrap">
-                  <span
-                    v-for="item in extra.list"
-                    class="text-base mr-1 mb-1 bg-stone-800 text-white px-1"
-                  >
-                    {{ item }}
-                  </span>
-                </div>
-              </div>
-            </template>
-          </div>
         </div>
       </div>
     </section>
 
-    <section
-      data-scroll-section
-      class="flex flex-col max-w-screen-2xl w-full mx-auto md:gap-6 pt-24 md:pt-24 px-6 md:px-12 md:gap-4"
-    >
-      <div class="flex items-center py-6">
-        <h1 class="text-3xl md:text-5xl font-bold text-amber-600">
-          Work experience
-        </h1>
-      </div>
-
-      <div class="flex flex-col text-stone-900">
-        <div
-          v-for="experience in listOfExperience"
-          class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-12 py-6 border-b border-stone-900"
-        >
-          <div class="flex flex-col">
-            <h3 class="text-xl md:text-2xl">
-              <span>
-                {{ experience.company }}
-                <span class="opacity-60 text-base font-bold">
-                  ({{ experience.from }} - {{ experience.to }})
-                </span>
-              </span>
-            </h3>
-
-            <span class="text-base md:text-lg">{{ experience.role }}</span>
+    <section class="divide-y divide-black">
+      <h2>Professional Experience</h2>
+      <div class="flex flex-col gap-2 pt-2">
+        <div v-for="experience in listOfExperience">
+          <h3>{{ experience.company }}</h3>
+          <div class="flex items-end">
+            <h4>{{ experience.role }}</h4>
+            <div class="flex-1"></div>
+            <h4 class="text-right">
+              {{ experience.from }} - {{ experience.to }}
+            </h4>
           </div>
-
-          <div class="flex flex-col">
-            <p class="text-lg" v-html="experience.note"></p>
-          </div>
+          <ul>
+            <li v-for="note in experience.listOfNote">{{ note }}</li>
+          </ul>
         </div>
       </div>
     </section>
 
-    <section
-      data-scroll-section
-      class="flex flex-col max-w-screen-2xl w-full mx-auto md:gap-6 pt-24 md:pt-24 px-6 md:px-12 md:gap-4"
-    >
-      <div class="flex items-center py-6">
-        <h1 class="text-3xl md:text-5xl font-bold text-amber-600">
-          What they say about me
-        </h1>
-      </div>
-
-      <div
-        class="grid md:grid-cols-1 lg:grid-cols-2 gap-12 md:gap-12 py-12 text-stone-900"
-      >
-        <div
-          v-for="testimonial in listOfTestimonial"
-          class="flex flex-col flex-1 border-l border-stone-900 pl-6"
-        >
-          <h3 class="text-base md:text-lg">
-            <span>{{ testimonial.text }}</span>
-          </h3>
-
-          <div class="flex flex-col pt-6">
-            <span class="text-base font-semibold">
-              {{ testimonial.name }}
-            </span>
-            <span class="text-sm">
-              {{ testimonial.position }}
-            </span>
+    <section class="divide-y divide-black">
+      <h2>Testimonials</h2>
+      <div class="flex flex-col gap-2 pt-2">
+        <div v-for="testimonial in listOfTestimonial">
+          <div class="flex items-end">
+            <h3>{{ testimonial.name }}</h3>
+            <div class="flex-1"></div>
+            <h4 class="text-right">{{ testimonial.position }}</h4>
           </div>
+          <p v-html="testimonial.text"></p>
         </div>
       </div>
     </section>
@@ -195,47 +98,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from 'vue'
-import LocomotiveScroll from 'locomotive-scroll'
-
-const container = ref()
-
-onMounted(() => {
-  const scroll = new LocomotiveScroll({
-    el: container.value,
-    smooth: true,
-  })
-})
-
-const listOfNavItem = [
-  {
-    label: 'Intro',
-    id: 'intro',
-  },
-  {
-    label: 'Skills',
-    id: 'skills',
-  },
-  {
-    label: 'Experience',
-    id: 'experience',
-  },
-  {
-    label: 'Testimonials',
-    id: 'testimonials',
-  },
-  {
-    label: 'Contact',
-    id: 'contact',
-  },
-]
-
 const listOfSkills = [
   {
     title: 'Full stack Development',
-    listOfDescription: [
-      'Throughout my career, I have successfully executed numerous projects, from developing web/mobile/hybrid applications, building full SSR e-commerce platforms or creating custom website solutions. I have explored and perfected various aspects of the web development, gaining extensive knowledge in web applications and API solutions.',
-    ],
+    description:
+      'The majority of my working hours have been dedicated to designing, developing, and managing different web and mobile solutions. I am eager to build and perfect various aspects of web applications, managmenent tools, micorservices or custom solutions.',
     listOfExtra: [
       {
         title: 'Commonly used:',
@@ -252,16 +119,15 @@ const listOfSkills = [
         ],
       },
       {
-        title: 'Also interested in',
-        list: ['Rust', 'Java/Kotlin', 'Go', 'Python'],
+        title: 'Also interested in: ',
+        list: ['Rust', 'Java/Kotlin', 'Go'],
       },
     ],
   },
   {
     title: 'Solution Architecture',
-    listOfDescription: [
-      'As a self-motivated and detail-oriented professional, I am deeply interested in developing high-end solutions by implementing scalable, reliable, and cost-effective strategies utilizing AWS services.',
-    ],
+    description:
+      'As a dedicated and detail-oriented professional, I am passionate about developing robust and cost-effective strategies using AWS services.',
     listOfExtra: [
       {
         title: 'Commonly used:',
@@ -285,10 +151,13 @@ const listOfExperience = [
   {
     company: 'C-BOX',
     from: '2022',
-    to: '...',
-    role: 'Lead developer & architect and IoT engineer',
+    to: 'Present',
+    role: 'Lead developer & IoT engineer',
     devTeamSize: '1',
-    note: `Developed hybrid applications (Web, iOS, Android) using Vue.js with TypeScript, Docker, Node.js, and AWS services such as ECS and Amplify. Additionally, created a dynamic system for locker stations integrated with hybrid application, utilizing AWS IoT Core, Greengrass, Python, SQS, MQTT, Rust, and Node.js with TypeScript.`.trim(),
+    listOfNote: [
+      'Designed and developed end-to-end system for urban smart locker stations, ensuring reliable integration with various web and mobile solutions, utilizing AWS IoT Core, Greengrass, Python, SQS, MQTT, Rust, and Node.js with TypeScript.',
+      'Developed hybrid application (iOS, Android, WEB) using Vue.js with TypeScript, Docker, Node.js, and AWS services such as ECS and Amplify',
+    ],
   },
   {
     company: 'Dermtest',
@@ -296,7 +165,10 @@ const listOfExperience = [
     to: '2024',
     role: 'Lead front-end developer',
     devTeamSize: '1-3',
-    note: `Participated in developing an application for dermatologists and general doctors using Angular with TypeScript, and AWS services. Additionally, designed and created a hybrid mobile application (Web, iOS, Android) for psoriasis patients using Node.js, Capacitor, Angular with TypeScript, and AWS services.`.trim(),
+    listOfNote: [
+      'Participated in developing an application for dermatologists and general doctors using Angular with TypeScript, and AWS services.',
+      'Designed and created a hybrid mobile application for psoriasis patients using Node.js, Capacitor, Angular with TypeScript, and AWS services.',
+    ],
   },
   {
     company: 'Flowit',
@@ -304,7 +176,10 @@ const listOfExperience = [
     to: '2022',
     role: 'Full Stack Developer',
     devTeamSize: '2-6',
-    note: `Led front-end development for tradehouse.ee and its management system. Also contributed as a front-end developer for the Derossi Marine CMS system, utilizing Odoo, Python, Node.js, PostgreSQL, and Vue.js.`.trim(),
+    listOfNote: [
+      'Led front-end development for tradehouse.ee and its management system.',
+      'Contributed as a front-end developer for the Derossi Marine CMS system, utilizing Odoo, Python, Node.js, PostgreSQL, and Vue.js.',
+    ],
   },
   {
     company: 'CostPocket',
@@ -312,7 +187,10 @@ const listOfExperience = [
     to: '2022',
     role: 'Full Stack Developer',
     devTeamSize: '2-5',
-    note: `Cornerstone of my developer journey, having designed and developed multiple web applications, a primary hybrid application, and various custom web solutions. Developed dozens of custom solutions to address diverse requirements.`.trim(),
+    listOfNote: [
+      'Developed multiple web applications, a primary hybrid application, and various custom web solutions.',
+      'Designed and developed an application for digitizing receipts and cost documents, processing over 30 000 documents per month.',
+    ],
   },
 ]
 
@@ -343,13 +221,4 @@ const listOfTestimonial = [
     text: 'I have worked with Kristjan on different projects and he has done some pages for me. He is that type of person who just decides that he will achieve something and he will. His learning capability and will do develop is extremely strong. He works hard but he needs challenges to feel motivated. He suits for organisations who look for creative and ambitions people not so much to organisations who need someone to carry out repetitive tasks. It has been a pleasure!',
   },
 ]
-
-const listOfContact = [
-  {
-    label: 'LINKEDIN',
-    href: 'https://www.linkedin.com/in/kristjan-suursoho-a475ba166/',
-  },
-]
 </script>
-
-<style></style>
